228c8b102906a3eac71645d329ee7ecdc2086c9e
Bashir <bash.arslanaliev@gmail.com>
2021-03-21 12:24:08 +0300

    Initial Commit

---
+++ MyPlan/MyPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlanApp.swift
+//  MyPlan
+//
+//  Created by Башир Арсланалиев on 21.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
